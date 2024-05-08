85ad121aa2a6d3ef2e365a2396ac08befc8476c7
NikhilK512 <nikhinikhil@kalvium.community@kalvium>
2024-05-08 17:15:28 +0530

    Uncommented the plugins from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react()],
-  base: 'lab-react-to-do-list-boilerplate',
+  plugins: [react()],
+  base: 'Kalvium-Note',
 })
